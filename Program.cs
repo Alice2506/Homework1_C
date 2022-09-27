74877f901d66db90c2c9cfbd93fd829a5d457981
Alice2506 <ralko84@mail.ru>
2022-09-27 22:38:37 +0300

    info about main commands added

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int number = 67;
+System.Console.WriteLine(number);
+string n = number.ToString();
+Console.WriteLine("Введите ваше имя");
+string name = Console.ReadLine();       // Отдает все только строкой
+Console.WriteLine("Введите чисо a ");
+int a = int.Parse(Console.ReadLine());  
+Console.WriteLine("Введите чисо b ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(a + b); 
+
+if (a > b)
+{
+Console.WriteLine("Число a больше числа b");
+} else {
+    if (a < b) {
+        Console.WriteLine("Число b больше числа a");
+    } else {
+        Console.WriteLine("Число a равно числу b");
+    }
+}
