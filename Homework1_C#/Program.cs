28da6ef3d2f5c2b18fa95b943255a7507d2629e7
Alice2506 <ralko84@mail.ru>
2022-09-30 16:33:06 +0300

    task 1 added

--- Homework1_C#/Program.cs
+++ Homework1_C#/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine(a);
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(b);
+if(a > b) {
+    Console.WriteLine("Максимальное число равно " + a);
+} else {
+    Console.WriteLine("Максимальное число равно " + b);
+}
+
+
+
