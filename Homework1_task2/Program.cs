c5f65a1fd07901b1fe72fc0a3e7c97743292df6e
Alice2506 <ralko84@mail.ru>
2022-10-01 20:46:13 +0300

    task 2 added

---
+++ Homework1_task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine(a);
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(b);
+Console.WriteLine("Введите число c: ");
+int c = int.Parse(Console.ReadLine());
+Console.WriteLine(c);
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine("Максимальное число равно " + max);
+
+
+
+
+
